0da954e7d9078524f0aa03a227dce53b0b2e876f
Russell-Tsai <vanilla.love10@gmail.com>
2018-01-03 16:42:59 +0800

    Initial Commit

---
+++ Facade/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Facade
+//
+//  Created by APP Dimerco on 03/01/2018.
+//  Copyright © 2018 APP Dimerco. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
